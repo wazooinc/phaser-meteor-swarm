0f84b8bc03b6f40d87ffe697eb53734a7faf50db
erikyuzwa <erikyuzwa@gmail.com>
2024-07-21 20:48:58 -0600

    feat: add timer for blinking text

--- scenes/MainMenuScene.js
+++ scenes/MainMenuScene.js
@@ -1,4 +1,5 @@
 import Phaser from 'phaser'
+import Meteor from '../entities/Meteor'
 
 export default class MainMenuScene extends Phaser.Scene {
 
@@ -19,19 +20,60 @@ export default class MainMenuScene extends Phaser.Scene {
     .add
     .bitmapText(width /2, height / 2 - 50, 'arcade', 'Meteor Swarm', 40)
     .setOrigin(0.5)
+    .setDepth(1)
 
-    const startText = this
+    this.startText = this
     .add
     .bitmapText(width / 2, height / 2 + 50, 'arcade', 'Press Space to Start', 30)
     .setOrigin(0.5)
+    .setDepth(1)
+
+    this
+    .add
+    .bitmapText(width / 2, height - 100, 'arcade', 'Made with Phaser', 18)
+    .setOrigin(0.5)
+    .setDepth(1)
+
+
+    this.startTextVisible = true
+    const timer = this.time.addEvent({
+      delay: 800,                // ms
+      callback: () => {
+        this.startTextVisible = !this.startTextVisible
+        this.startText.setVisible(this.startTextVisible)
+      },
+      callbackScope: this,
+      loop: true
+    })
+
+    // generate our meteors
+    this.meteorGroup = this.physics.add.group()
+    this.meteorArray = []
+
+    for (let i = 0; i < 10; i++) {
+      const meteor = new Meteor(this, 300, 300)
+
+      const xPos = Phaser.Math.RND.between(0, 800)
+      const yPos = Phaser.Math.RND.between(0, 600)
+      meteor.setPosition(xPos, yPos)
+      meteor.setActive(true)
+      meteor.setVisible(true)
+
+      this.meteorGroup.add(meteor, true)
+      this.meteorArray.push(meteor)
+    }
     
 
     this.cursors = this.input.keyboard.createCursorKeys()
   }
 
-  update() {
+  update(time, delta) {
     if (this.cursors.space.isDown) {
       this.scene.switch('play-scene')
+    }
+
+    for (const meteor of this.meteorArray) {
+      meteor.update(time, delta)
     }
   }
 }
